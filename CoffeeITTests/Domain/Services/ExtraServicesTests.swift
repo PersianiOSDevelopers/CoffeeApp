945d9c0ec3fa92fa20513c3de7d6e04bbc220626
amir tutunchi <amirbt@live.com>
2021-08-08 21:14:40 +0430

    remove test template file and extra spaces

--- CoffeeITTests/Domain/Services/ExtraServicesTests.swift
+++ CoffeeITTests/Domain/Services/ExtraServicesTests.swift
@@ -1,10 +1,3 @@
-//
-//  ExtraServicesTests.swift
-//  CoffeeITTests
-//
-//  Created by Amir Tutunchi on 8/7/21.
-//
-
 import XCTest
 @testable import CoffeeIT
 class ExtraServicesTests: XCTestCase {
